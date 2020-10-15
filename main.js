df291ad785aae92b93503d32ccfebe6a887de15c
SaiAnshuman <71771487+SaiAnshuman@users.noreply.github.com>
2020-10-15 09:07:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ menu_list_array = [ "Chicken Tandoori Pizza",
       htmldata="<section class='cards'>"
       for(var i=0;i<menu_list_array.length;i++){
 
-           htmldata=htmldata+'<div class="card">' +'<img src="images/pizzaImg.png"/>' +menu_list_array[i] + '</div>'
+           htmldata=htmldata+'<div class="card">' +'<img src="pizzaImg.png"/>' +menu_list_array[i] + '</div>'
 
       }
 
